ae28f2323f2ed226437fb425546d120d5f24f893
rmreshma72 <100831815+rmreshma72@users.noreply.github.com>
2022-04-06 18:58:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function submit(){
+  var name=document.getElementById("name").value;
+  var email=document.getElementById("email").value;
+  var phone=document.getElementById("phone").value;
+  alert(name+"\n"+email+"\n"+phone);
+}
